13788747309581188a658e49acfadd246dd55dc6
yazidkucrit <yazid.kucrit@gmail.com>
2021-10-21 19:35:13 +0700

    Add files via upload

---
+++ omni_selene.mk
@@ -0,0 +1,73 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 The TWRP Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator 
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Specify phone tech before including full_phone
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, build/target/product/embedded.mk)
+
+# Inherit Telephony packages
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit language packages
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit 64bit support
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+# A/B
+AB_OTA_PARTITIONS += \
+    boot \
+    system
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+# Boot control HAL
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.0-impl \
+    android.hardware.boot@1.0-service
+
+PRODUCT_PACKAGES += \
+    bootctrl.$(TARGET_BOARD_PLATFORM)
+    
+PRODUCT_STATIC_BOOT_CONTROL_HAL := \
+    bootctrl.$(TARGET_BOARD_PLATFORM) \
+    libgptutils \
+    libz \
+    libcutils
+    
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    cppreopts.sh \
+    update_engine \
+    update_verifier \
+    update_engine_sideload
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := selene
+PRODUCT_NAME := omni_selene
+PRODUCT_BRAND := xiaomi
+PRODUCT_MODEL := Redmi 10
+PRODUCT_MANUFACTURER := xiaomi
+PRODUCT_RELEASE_NAME := Redmi 10
